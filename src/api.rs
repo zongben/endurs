9d00152d9a26f7dbd78eac1be3f450087177e6af
ben <ben76020567@gmail.com>
2025-08-15 12:54:14 +0800

    basic elem control

--- src/api.rs
+++ src/api.rs
@@ -3,9 +3,41 @@ use std::rc::Rc;
 use anyhow::Result;
 
 use mlua::{Error, Lua, UserData};
-use thirtyfour::WebDriver;
+use thirtyfour::{By, WebDriver, WebElement};
 
 use crate::ENTRYPOINT_NAME;
+
+pub struct Element {
+    elem: WebElement,
+}
+
+impl UserData for Element {
+    fn add_methods<M: mlua::UserDataMethods<Self>>(methods: &mut M) {
+        methods.add_async_method("send_keys", |_, this, input: String| async move {
+            this.elem.send_keys(input).await.map_err(Error::external)?;
+            Ok(())
+        });
+
+        methods.add_async_method("click", |_, this, ()| async move {
+            this.elem.click().await.map_err(Error::external)?;
+            Ok(())
+        });
+
+        methods.add_async_method(
+            "find",
+            |lua, this, (by, value): (String, String)| async move {
+                let by = match by.as_str() {
+                    "id" => By::Id(value),
+                    "name" => By::Name(value),
+                    _ => return Err(Error::external(format!("{} is not supported", by))),
+                };
+                let elem = this.elem.find(by).await.map_err(Error::external)?;
+                let elem_userdata = lua.create_userdata(Element { elem });
+                Ok(elem_userdata)
+            },
+        );
+    }
+}
 
 pub struct Api {
     driver: Rc<WebDriver>,
@@ -17,6 +49,42 @@ impl UserData for Api {
             this.driver.goto(url).await.map_err(Error::external)?;
             Ok(())
         });
+
+        methods.add_async_method(
+            "find",
+            |lua, this, (by, value): (String, String)| async move {
+                let by = match by.as_str() {
+                    "id" => By::Id(value),
+                    "name" => By::Name(value),
+                    _ => return Err(Error::external(format!("{} is not supported", by))),
+                };
+
+                let elem = this.driver.find(by).await.map_err(Error::external)?;
+
+                let elem_userdata = lua.create_userdata(Element { elem });
+                Ok(elem_userdata)
+            },
+        );
+
+        methods.add_async_method(
+            "find_all",
+            |lua, this, (by, value): (String, String)| async move {
+                let by = match by.as_str() {
+                    "id" => By::Id(value),
+                    "name" => By::Name(value),
+                    _ => return Err(Error::external(format!("{} is not supported", by))),
+                };
+
+                let elems = this.driver.find_all(by).await.map_err(Error::external)?;
+
+                let t = lua.create_table()?;
+                for (i, elem) in elems.into_iter().enumerate() {
+                    t.set(i + 1, lua.create_userdata(Element { elem })?)?;
+                }
+
+                Ok(t)
+            },
+        );
 
         methods.add_async_method("close", |_, this, ()| async move {
             this.driver.close_window().await.map_err(Error::external)?;
--- tests/lua/index.lua
+++ tests/lua/index.lua
@@ -1,2 +1,9 @@
-endurs:goto("https://www.gamer.com.tw/")
-endurs:close()
+local e = endurs
+
+e:goto("https://www.google.com/")
+local query_input = e:find("id", "APjFqb")
+query_input:send_keys("test")
+
+local btn = e:find("name", "btnK")
+btn:click()
+e:close()
